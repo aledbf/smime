15ed5730c30b9a4de85b7da1be7e406644e0d1c5
Manuel de Brito Fontes <aledbf@gmail.com>
2018-08-03 16:39:42 -0400

    Add support for encrypt and decrypt

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-var util = require('util');
-var spawn = require('child_process').spawn;
+const util = require("util");
+const spawn = require("child_process").spawn;
+const intoStream = require('into-stream');
 
 // Expose methods.
 exports.sign = sign;
+exports.encrypt = encrypt;
+exports.decrypt = decrypt;
 
 /**
  * Sign a file.
@@ -18,46 +21,133 @@ exports.sign = sign;
  */
 
 function sign(options) {
-  return new Promise(function (resolve, reject) {
+  return new Promise((resolve, reject) => {
     options = options || {};
 
-    if (!options.content)
-      throw new Error('Invalid content.');
+    if (!options.content) {
+      throw new Error("Invalid content.");
+    }
 
-    if (!options.key)
-      throw new Error('Invalid key.');
+    if (!options.key) {
+      throw new Error("Invalid key.");
+    }
 
-    if (!options.cert)
-      throw new Error('Invalid certificate.');
+    if (!options.cert) {
+      throw new Error("Invalid certificate.");
+    }
 
-    var command = util.format(
-      'openssl smime -sign -text -signer %s -inkey %s -outform DER -binary',
+    let command = util.format(
+      "openssl smime -sign -text -signer %s -inkey %s -outform DER -binary",
       options.cert,
       options.key
     );
 
-    if (options.password)
-      command += util.format(' -passin pass:%s', options.password);
+    if (options.password) {
+      command += util.format(" -passin pass:%s", options.password);
+    }
 
-    var args = command.split(' ');
-    var child = spawn(args[0], args.splice(1));
+    const args = command.split(" ");
+    const child = spawn(args[0], args.splice(1));
 
-    var der = [];
+    const der = [];
 
-    child.stdout.on('data', function (chunk) {
+    child.stdout.on("data", chunk => {
       der.push(chunk);
     });
 
-    child.on('close', function (code) {
-      if (code !== 0)
-        reject(new Error('Process failed.'));
-      else
-        resolve({
-          child: child,
-          der: Buffer.concat(der)
-        });
+    child.on("close", code => {
+      if (code !== 0) {
+        return reject(new Error("Process failed."));
+      }
+
+      resolve({
+        child: child,
+        der: Buffer.concat(der)
+      });
     });
 
     options.content.pipe(child.stdin);
+  });
+}
+
+function encrypt(options) {
+  return new Promise((resolve, reject) => {
+    options = options || {};
+
+    if (!options.content) throw new Error("Invalid content.");
+
+    if (!options.keys) throw new Error("Invalid keys.");
+
+    const command = util.format(
+      "openssl smime -noattr -encrypt -aes-256-cbc -outform PEM -binary %s",
+      options.keys
+    );
+
+    const args = command.split(" ");
+    const child = spawn(args[0], args.splice(1));
+
+    let der = "";
+
+    child.stdout.on("data", chunk => {
+      der += chunk;
+    });
+
+    child.on("close", code => {
+      if (code !== 0) {
+        return reject(new Error("Process failed."));
+      }
+
+      return resolve({
+        child: child,
+        der: Buffer.from(der)
+      });
+    });
+
+    options.content.pipe(child.stdin);
+  });
+}
+
+function decrypt(options) {
+  return new Promise((resolve, reject) => {
+    options = options || {};
+
+    if (!options.content) {
+      return reject(new Error("Invalid content."));
+    }
+
+    if (!Buffer.isBuffer(options.content)) {
+      return reject(new Error("content is not a buffer."));
+    }
+
+    if (!options.key) {
+      return reject(new Error("Invalid key."));
+    }
+
+    const command = util.format(
+      "openssl smime -decrypt -inform PEM -inkey %s",
+      options.key
+    );
+
+    const args = command.split(" ");
+    const child = spawn(args[0], args.splice(1));
+
+    let der = "";
+
+    child.stdout.on("data", chunk => {
+      der += chunk;
+    });
+
+    child.on("close", code => {
+      if (code !== 0) {
+        reject(new Error("Process failed."));
+      } else {
+        resolve({
+          child: child,
+          content: Buffer.from(der)
+        });
+      }
+    });
+
+    intoStream(options.content).pipe(child.stdin);
   });
 }
--- test/test.js
+++ test/test.js
@@ -1,63 +1,153 @@
-var expect = require('chai').expect;
-var fs = require('fs');
-var path = require('path');
-var smime = require('../');
+const expect = require("chai").expect;
+const fs = require("fs");
+const path = require("path");
+const smime = require("../");
 
-describe('Smime', function () {
-  describe('#sign', function () {
-    it('should return an error if there is no content', function () {
-      return smime.sign({
-          key: path.join(__dirname, 'key.pem'),
-          cert: path.join(__dirname, 'certificate.pem')
+describe("Smime", () => {
+  describe("#sign", () => {
+    it("should return an error if there is no content", () => {
+      return smime
+        .sign({
+          key: path.join(__dirname, "key.pem"),
+          cert: path.join(__dirname, "certificate.pem")
         })
-        .catch(function (err) {
-          expect(err.message).to.equal('Invalid content.');
+        .catch(err => {
+          expect(err.message).to.equal("Invalid content.");
         });
     });
 
-    it('should return an error if there is no key', function () {
-      return smime.sign({
-          content: fs.createReadStream(path.join(__dirname, 'file-to-sign')),
-          cert: path.join(__dirname, 'certificate.pem')
+    it("should return an error if there is no key", () => {
+      return smime
+        .sign({
+          content: fs.createReadStream(path.join(__dirname, "file-to-sign")),
+          cert: path.join(__dirname, "certificate.pem")
         })
-        .catch(function (err) {
-          expect(err.message).to.equal('Invalid key.');
+        .catch(err => {
+          expect(err.message).to.equal("Invalid key.");
         });
     });
 
-    it('should return an error if there is no cert', function () {
-      return smime.sign({
-          content: fs.createReadStream(path.join(__dirname, 'file-to-sign')),
-          key: path.join(__dirname, 'key.pem')
+    it("should return an error if there is no cert", () => {
+      return smime
+        .sign({
+          content: fs.createReadStream(path.join(__dirname, "file-to-sign")),
+          key: path.join(__dirname, "key.pem")
         })
-        .catch(function (err) {
-          expect(err.message).to.equal('Invalid certificate.');
+        .catch(err => {
+          expect(err.message).to.equal("Invalid certificate.");
         });
     });
 
-    it('should sign a content', function () {
-      return smime.sign({
-          content: fs.createReadStream(path.join(__dirname, 'file-to-sign')),
-          key: path.join(__dirname, 'key.pem'),
-          cert: path.join(__dirname, 'certificate.pem')
+    it("should sign a content", () => {
+      return smime
+        .sign({
+          content: fs.createReadStream(path.join(__dirname, "file-to-sign")),
+          key: path.join(__dirname, "key.pem"),
+          cert: path.join(__dirname, "certificate.pem")
         })
-        .then(function (res) {
-          expect(res).to.have.property('der');
-          expect(res).to.have.property('child');
+        .then(res => {
+          expect(res).to.have.property("der");
+          expect(res).to.have.property("child");
         });
     });
 
-    it('should work with password', function () {
-      return smime.sign({
-          content: fs.createReadStream(path.join(__dirname, 'file-to-sign')),
-          key: path.join(__dirname, 'key.pem'),
-          cert: path.join(__dirname, 'certificate.pem'),
-          password: 'x'
+    it("should work with password", () => {
+      return smime
+        .sign({
+          content: fs.createReadStream(path.join(__dirname, "file-to-sign")),
+          key: path.join(__dirname, "key.pem"),
+          cert: path.join(__dirname, "certificate.pem"),
+          password: "x"
         })
-        .then(function (res) {
-          expect(res).to.have.property('der');
-          expect(res).to.have.property('child');
+        .then(res => {
+          expect(res).to.have.property("der");
+          expect(res).to.have.property("child");
         });
+    });
+  });
+
+  describe("#encrypt", () => {
+    it("should return an error if there is no content", () => {
+      return smime
+        .encrypt({
+          keys: [path.join(__dirname, "key.pem")]
+        })
+        .catch(err => {
+          expect(err.message).to.equal("Invalid content.");
+        });
+    });
+
+    it("should return an error if there is no keys", () => {
+      return smime
+        .encrypt({
+          content: fs.createReadStream(path.join(__dirname, "file-to-sign"))
+        })
+        .catch(err => {
+          expect(err.message).to.equal("Invalid keys.");
+        });
+    });
+
+    it("should encrypt a content", () => {
+      return smime
+        .encrypt({
+          content: fs.createReadStream(path.join(__dirname, "file-to-sign")),
+          keys: [path.join(__dirname, "certificate.pem")]
+        })
+        .then(res => {
+          expect(res).to.have.property("der");
+          expect(res).to.have.property("child");
+        });
+    });
+  });
+
+  describe("#decrypt", () => {
+    it("should return an error if there is no content", () => {
+      return smime
+        .decrypt({
+          keys: [path.join(__dirname, "key.pem")]
+        })
+        .catch(err => {
+          expect(err.message).to.equal("Invalid content.");
+        });
+    });
+
+    it("should return an error if there is no keys", () => {
+      return smime
+        .decrypt({
+          content: Buffer.from("")
+        })
+        .catch(err => {
+          expect(err.message).to.equal("Invalid key.");
+        });
+    });
+
+    it("should return an error if content is not a buffer", () => {
+      return smime
+        .decrypt({
+          content: fs.createReadStream(path.join(__dirname, "file-to-sign"))
+        })
+        .catch(err => {
+          expect(err.message).to.equal("content is not a buffer.");
+        });
+    });
+
+    it("should decrypt a content", () => {
+      const enc = smime.encrypt({
+        content: fs.createReadStream(path.join(__dirname, "file-to-sign")),
+        keys: [path.join(__dirname, "certificate.pem")]
+      });
+
+      enc.then((res) => {
+        return smime.decrypt({
+          content: res.der,
+          key: path.join(__dirname, "key.pem")
+        });
+      }).then((res) => {
+        expect(res).to.have.property("content");
+        expect(res).to.have.property("child");
+        const content = fs.readFileSync(path.join(__dirname, "file-to-sign"));
+        expect(res.content.toString()).to.be.equal(content.toString());
+      });
     });
   });
 });
